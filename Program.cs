b52107afeac5b7810982870456c4f8c3958e7625
katib03 <ibkocak03@gmail.com>
2024-01-08 19:18:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    // Bu proje data type convertion çevrimleri üzerinedir.
+    // konular: 
+
+
+
+    private static void Main(string[] args)
+    {
+        #region
+            
+        #endregion
+
+
+
+    }
+}
